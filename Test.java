3b8bfbfc8f40090e2c8cea36636543cb22417032
Deemo <rhrh962@gmail.com>
2022-05-19 18:12:35 -0700

    ASDF

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ import java.util.jar.Manifest;
 public class test {
     public static void main(String args[]) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
